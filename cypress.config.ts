05cf6b25e07245dcee4473e1eb8ca26050bc4cc9
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-10-17 21:39:59 +0600

    cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    specPattern: 'cypress/integration/*.cy.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
