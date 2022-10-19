657bcde298198dafdb2f077feb99e880b921fd4f
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-10-19 20:22:53 +0600

    inventory cart length

--- cypress/integration/inventory.cy.ts
+++ cypress/integration/inventory.cy.ts
@@ -1,5 +1,5 @@
 describe("inventory", () => {
-  it("should NOT visit", () => {
+  it("cart count", () => {
     cy.visit("").then(() => {
       cy.get('[data-test="username"]').focus().type("standard_user");
       cy.get('[data-test="password"]').focus().type("secret_sauce");
@@ -30,6 +30,50 @@ describe("inventory", () => {
             });
         }
       );
+    });
+  });
+
+  it("cart price check", () => {
+    cy.visit("").then(() => {
+      cy.get('[data-test="username"]').focus().type("standard_user");
+      cy.get('[data-test="password"]').focus().type("secret_sauce");
+      cy.get('[data-test="login-button"]').click();
+      cy.wait(2000);
+
+      cy.get('*[class^="btn btn_primary btn_small btn_inventory"]')
+        .first()
+        .click();
+      cy.get('*[class^="btn btn_secondary btn_small btn_inventory"]')
+        .parent()
+        .children()
+        .first()
+        .invoke("text")
+        .then((priceText) => {
+          cy.get('*[class^="shopping_cart_badge"]').click();
+          cy.wait(2000);
+          cy.contains(priceText);
+        });
+    });
+  });
+
+  it("cart length", () => {
+    cy.visit("").then(() => {
+      cy.get('[data-test="username"]').focus().type("standard_user");
+      cy.get('[data-test="password"]').focus().type("secret_sauce");
+      cy.get('[data-test="login-button"]').click();
+      cy.wait(2000);
+
+      cy.get('*[class^="btn btn_primary btn_small btn_inventory"]')
+        .click({
+          multiple: true,
+        })
+        .then((itemOrdered) => {
+          cy.get('*[class^="shopping_cart_badge"]').click();
+          
+          cy.get('[class=cart_item]').then((listing) => {
+            expect(listing).to.have.length(Cypress.$(itemOrdered).length);
+          });
+        });
     });
   });
 });
