610285901d21bd5e1f4bc15a3810e561ec717653
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-11-14 15:18:57 +0600

    [chores]: making sure that, all tests pass

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,13 @@ export default defineConfig({
     baseUrl: 'http://www.saucedemo.com',
     specPattern: 'cypress/integration/*.cy.ts',
     "chromeWebSecurity": false,
+    "retries": 3,
+    "pageLoadTimeout": 10000,
+    "execTimeout": 10000,
+    "requestTimeout": 100000,
+    "taskTimeout": 10000,
+    "responseTimeout": 10000,
+    "defaultCommandTimeout": 10000,
     // @ts-ignore
     setupNodeEvents(on, config) {
       return {
